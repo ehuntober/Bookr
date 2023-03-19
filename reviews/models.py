dacca30e6eda2f0468ab71130c5b9f52b8f4e213
ehuntober <ehuntercrypto@gmail.com>
2023-03-19 08:41:12 +0000

    new model created

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Publisher(models.Model):
+    """ A company that publishes books. """
+    name = models.CharField(max_length=50,help_text="The name of the Publisher.")
+    website = models.URLField(help_text="The Publisher website")
+    email = models.EmailField()
+    
+
+class Book(models.Model):
+    
--- reviews/views.py
+++ reviews/views.py
@@ -5,4 +5,5 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-    return render(request,"base.html")
+    name = "world"
+    return render(request,"base.html", {"name":name})
