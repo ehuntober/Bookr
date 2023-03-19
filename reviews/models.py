950b2c5a69eae9b42bd7733e62e95f670da1a82e
ehuntober <ehuntercrypto@gmail.com>
2023-03-19 10:12:07 +0000

    models fixed

--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
 
@@ -8,6 +9,8 @@ class Publisher(models.Model):
     name = models.CharField(max_length=50,help_text="The name of the Publisher.")
     website = models.URLField(help_text="The Publisher website")
     email = models.EmailField()
+    
+    def _
     
 
 class Book(models.Model):
@@ -25,4 +28,24 @@ class Contributor(models.Model):
     last_names =models.CharField(max_length=50,help_text="The contributor's last name or names")
     email = models.EmailField(help_text="The contact email for the contributor.")
 
-class BookContributors(models)
+class BookContributors(models.Model):
+    class ContributionRole(models.TextChoices):
+        AUTHOR = "AUTHOR","Author"
+        CO_AUTHOR = "CO_AUTHOR" , "Co-Author"
+        EDITOR = "EDITOR" , "Editor"
+        
+    
+    book = models.ForeignKey(Book,on_delete=models.CASCADE)
+    contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
+    role = models.CharField(verbose_name="This role this contributor had in the book.", \
+        choices=ContributionRole.choices, max_length=20)
+    
+
+class Review(models.Model):
+    content = models.TextField(help_text="The Review text.")
+    rating = models.IntegerField( help_text="The rating the reviewer has given")
+    date_created = models.DateTimeField(auto_now_add=True,help_text="The date and time the review was created")
+    date_edited = models.DateTimeField(null=True,help_text="The date and time the review was last edited.")
+    
+    creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,help_text="The Book that this review is for")
