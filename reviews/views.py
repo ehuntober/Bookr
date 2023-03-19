48a53bdc57802ed297b9cea4dee8978ea0cf9f9b
ehuntober <ehuntercrypto@gmail.com>
2023-03-19 07:53:17 +0000

    new file add

--- reviews/views.py
+++ reviews/views.py
@@ -5,5 +5,4 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-    name = request.get("name") or "world"
-    return HttpResponse("Hello, {}".format(name))
+    return render(request,"base.html")
