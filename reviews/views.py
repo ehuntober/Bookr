6c2c6d4ef0dbaea5422385679d4c5fbfbd3e8cf6
ehuntober <ehuntercrypto@gmail.com>
2023-03-19 17:23:29 +0000

    view modified

--- reviews/views.py
+++ reviews/views.py
@@ -3,11 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 from django.views.generic import TemplateView
 
-class HomePage(TemplateView):
-    TemplateVeiw ='home_page.html'
-
-
-
-def index(request):
-    name = "world"
-    return render(request,"base.html", {"name":name})
+def welcome_view(request):
+    return render(request,'base.html')
