5883a61beda8f3eec42d23fdaeeb0caa2267192f
ehuntober <ehuntercrypto@gmail.com>
2023-03-20 13:50:04 +0000

    added book list to the template

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,9 @@
-from django.contrib import admin 
-from django.urls import path 
-from . import views 
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('',views.welcome_view,name='index'),
-    path('books/', views.book_list , name='book_list')
+    path('', views.index),
+    path('books/', views.book_list, name='book_list'),
+    path('books/<int:pk>/', views.book_detail, name='book_detail'),
+    path('book-search/', views.book_search, name='book_search')
 ]
