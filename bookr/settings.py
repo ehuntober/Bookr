6fdfe10d0902e718cea4c453c76d6c740039cf3c
ehuntober <ehuntercrypto@gmail.com>
2023-03-20 13:27:27 +0000

    new file added

--- bookr/settings.py
+++ bookr/settings.py
@@ -52,11 +52,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'bookr.urls'
-
+# [os.path.join(BASE_DIR,'templates')],
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
