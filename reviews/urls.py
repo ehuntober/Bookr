51e7362e648008dd25db58191d788bb13a227baa
ehuntober <ehuntercrypto@gmail.com>
2023-03-26 19:49:50 +0000

    created a new urls for two link

--- reviews/urls.py
+++ reviews/urls.py
@@ -10,7 +10,10 @@ urlpatterns = [
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.book_detail, name='book_detail'),
     path('book-search/', views.book_search, name='book_search'),
+    
     # path('',include('reviews.urls'))
+    path('publishers/<int:pk>/', views.publisher_edit,name="publisher_edit"),
+    path('publishers/new/', views.publisher_edit,name='publisher_create')
 ]
 
 #   path('admin/',admin_site.urls),
