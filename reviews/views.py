b413dce8a3f0475ff615df44d208a4cd06e1e44e
ehuntober <ehuntercrypto@gmail.com>
2023-03-31 15:00:49 +0000

    new image added

--- reviews/views.py
+++ reviews/views.py
@@ -5,6 +5,7 @@ from django.core.files.images import ImageFile
 from django.shortcuts import render, get_object_or_404 , redirect
 from django.contrib import messages
 from django.utils import timezone
+
 from .models import Book
 from .utils import average_rating
 from .forms import SearchForm , PublisherForm , ReviewForm
