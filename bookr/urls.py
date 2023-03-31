fd3690f277e6bbe6d4a0bc6307167be09f90164e
ehuntober <ehuntercrypto@gmail.com>
2023-03-31 16:42:57 +0000

    urls added

--- bookr/urls.py
+++ bookr/urls.py
@@ -31,8 +31,8 @@ urlpatterns = [
 ]
 
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
