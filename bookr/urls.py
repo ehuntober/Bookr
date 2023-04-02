e9da144380d3c0030b14c88b217900625fd36bc0
ehuntober <ehuntercrypto@gmail.com>
2023-04-02 16:36:31 +0000

    urls bookr modified

--- bookr/urls.py
+++ bookr/urls.py
@@ -30,10 +30,12 @@ from django.conf import settings
     
 # ]
 
+from bookr.view import profile
 
 urlpatterns = [
     path('accounts/', include(('django.contrib.auth.urls', 'auth'),
          namespace='accounts')),
+    path('accounts/profile', profile, name='profile'),
     path('admin/', admin.site.urls),
     path('', include('reviews.urls'))
 ]
