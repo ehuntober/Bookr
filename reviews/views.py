7139410c48722e5cbd7442eed2e18c2460ae2a26
ehuntober <ehuntercrypto@gmail.com>
2023-04-06 17:18:37 +0000

    views modified

--- reviews/views.py
+++ reviews/views.py
@@ -18,8 +18,7 @@ from django.contrib.auth.decorators import permission_required
 def index(request):
     return render(request, "reviews/base.html")
 
-nw
-and ole
+
 
 def book_search(request):
     search_text = request.GET.get("search", "")
