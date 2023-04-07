40afe01004f8cf3186ed0a2394f698b8ea427c10
ehuntober <ehuntercrypto@gmail.com>
2023-04-07 16:55:38 +0000

    book management app created

--- bookr/settings.py
+++ bookr/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reviews'
+    'reviews',
+    'book_management'
     # reiew
 ]
 
