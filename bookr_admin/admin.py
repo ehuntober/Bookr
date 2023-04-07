0c50314eb710d7ad09488ed6b74312be6d05702d
ehuntober <ehuntercrypto@gmail.com>
2023-04-07 14:33:34 +0000

    advance admin profile created

--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -6,15 +6,21 @@
 
 # admin_site = BookrAdmin(name='bookr_admin')
 # admin_site.register(User)
-
 from django.contrib import admin
 from django.template.response import TemplateResponse
 from django.urls import path
 
 class BookrAdmin(admin.AdminSite):
     site_header = "Bookr Administration"
-    logout_template = "admin/logout.html"
 
+    def profile_view(self, request):
+        request.current_app = self.name
+        context = self.each_context(request)
+        return TemplateResponse(request, "admin/admin_profile.html", context)
 
-def profile_view(self,request):
-    
+    def get_urls(self):
+        urls = super().get_urls()
+        url_patterns = [
+            path("admin_profile", self.admin_view(self.profile_view))
+        ]
+        return urls + url_patterns
