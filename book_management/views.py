86bcf0ec5ad61b54519357d606c46e3367c14781
ehuntober <ehuntercrypto@gmail.com>
2023-04-07 17:27:27 +0000

    views modified

--- book_management/views.py
+++ book_management/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.views.generic.edit import FormView 
+from django.views import View 
+
+from .forms import BookForm
+
+class BookRecordFormView(FormView):
+    template_name = 'book_form.html'
+    form_class = BookForm
+    success_url = '/book_management/entry_success'
+    
+    def form_valid(self,form):
+        form.save()
+        return super().form_valid(form)
+    
+
+class FormSuccessView(View):
+    def get(self,request,*args,**kwargs):
+        return HttpResponse("Book record saved successfully")
