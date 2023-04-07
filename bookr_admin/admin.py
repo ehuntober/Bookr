943de23e7cdebbb98a625895ad8a3faa5bd5f907
ehuntober <ehuntercrypto@gmail.com>
2023-04-07 13:31:04 +0000

    Admin costomize ongoing

--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -1,8 +1,13 @@
-from django.contrib import admin
+# from django.contrib import admin
 # from django.contrib.auth.admin import User
+
+# class BookrAdmin(admin.AdminSite):
+#     site_header = "Bookr Administration"
+
+# admin_site = BookrAdmin(name='bookr_admin')
+# admin_site.register(User)
+
+from django.contrib import admin
 
 class BookrAdmin(admin.AdminSite):
     site_header = "Bookr Administration"
-
-admin_site = BookrAdmin(name='bookr_admin')
-# admin_site.register(User)
