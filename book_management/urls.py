4c54a3ecd1e4db2328870eea86c1989134122392
ehuntober <ehuntercrypto@gmail.com>
2023-04-08 06:55:30 +0000

    delete book url created

--- book_management/urls.py
+++ book_management/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import BookRecordFormView, FormSuccessView , BookUpdateView
+from .views import BookRecordFormView, FormSuccessView , BookUpdateView ,BookDeleteView
 
 urlpatterns = [
     path('new_book_record', BookRecordFormView.as_view(), name='book_record_form'),
     path('entry_success', FormSuccessView.as_view(), name='form_success'),
-    path('book_record_update/<int:pk>',BookUpdateView.as_view(),name="book_update")
+    path('book_record_update/<int:pk>',BookUpdateView.as_view(),name="book_update"),
+    path('book_record_delete/<int:pk>', BookDeleteView.as_view(), name="book_delete")
 ]
