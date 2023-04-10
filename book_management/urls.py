d4af67d314936037f1b844c1ac48d857054073ff
ehuntober <ehuntercrypto@gmail.com>
2023-04-10 20:26:24 +0000

    profile template modified

--- book_management/urls.py
+++ book_management/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import BookRecordFormView, FormSuccessView , BookUpdateView ,BookDeleteView , BookRecordDetailView
 
 urlpatterns = [
-    path('new_book_record', BookRecordFormView.as_view(), name='book_record_form'),
-    path('entry_success', FormSuccessView.as_view(), name='form_success'),
-    path('book_record_update/<int:pk>',BookUpdateView.as_view(),name="book_update"),
-    path('book_record_delete/<int:pk>', BookDeleteView.as_view(), name="book_delete"),
-    path('book_record_detail/<int:pk>', BookRecordDetailView.as_view(), name='book_detail')
+#     path('new_book_record', BookRecordFormView.as_view(), name='book_record_form'),
+#     path('entry_success', FormSuccessView.as_view(), name='form_success'),
+#     path('book_record_update/<int:pk>',BookUpdateView.as_view(),name="book_update"),
+#     path('book_record_delete/<int:pk>', BookDeleteView.as_view(), name="book_delete"),
+#     path('book_record_detail/<int:pk>', BookRecordDetailView.as_view(), name='book_detail')
 ]
