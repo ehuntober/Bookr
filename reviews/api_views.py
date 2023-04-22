61d5b051a979d57893778499004f3fbec589f5cf
ehuntober <ehuntercrypto@gmail.com>
2023-04-22 18:26:01 +0000

    api view serializer

--- reviews/api_views.py
+++ reviews/api_views.py
@@ -2,8 +2,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response 
 
 from .models import Book 
+from .seri
 
 @api_view()
 def first_api_view(request):
     num_books = Book.objects.count()
     return Response({"num_books": num_books})
+
+
+@api_view()
+def all_books(request):
+    books = Book
