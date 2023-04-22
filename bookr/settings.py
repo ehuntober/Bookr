b941020d76ce4934ab46fbb01fedf61553f0112b
ehuntober <ehuntercrypto@gmail.com>
2023-04-22 16:49:50 +0000

    setting error modified

--- bookr/settings.py
+++ bookr/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reviews',
-    'book_management'
+    'book_management',
     # reiew
     'restframework'
 ]
