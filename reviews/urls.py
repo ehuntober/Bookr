86bc844bf62e2e022fa1a48fb7f6621d9d6e50cb
ehuntober <ehuntercrypto@gmail.com>
2023-04-25 14:42:50 +0000

    new url added

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,15 @@ from reviews.admin import admin_site
 from django.urls import path , include
 from . import views , api_views
 
+
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'books', api_views.BookViewSet)
+router.register(r'reviews', api_views.ReviewViewSet)
+
+
 # new url
 urlpatterns = [
     path('admin/', admin_site.urls),
@@ -21,6 +30,9 @@ urlpatterns = [
     # path('api/all_books/', api_views.all_books, name='all_books'),
     path('api/all_books/', api_views.AllBooks.as_view(),name='all_books'),
     path('api/contributors/', api_views.ContributorView.as_view(), name='contributors'),
+    
+    path('api/login', api_views.Login.as_view(), name='login'),
+    path('api/', include((router.urls, 'api'))),
     
     
 ]
