e40096ae91edb1879e96fce0e0bf2c0883ba8889
ehuntober <ehuntercrypto@gmail.com>
2023-04-25 13:05:06 +0000

    creating with meta models

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -3,17 +3,17 @@
 from rest_framework import serializers
 from  .models import Contributor
 
-class PublisherSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    website = serializers.URLField()
-    email = serializers.EmailField()
+# class PublisherSerializer(serializers.Serializer):
+#     name = serializers.CharField()
+#     website = serializers.URLField()
+#     email = serializers.EmailField()
     
     
-class BookSerializer(serializers.Serializer):
-    title = serializers.CharField()
-    publication_date = serializers.DateField()
-    isnb = serializers.CharField()
-    publisher = PublisherSerializer()
+# class BookSerializer(serializers.Serializer):
+#     title = serializers.CharField()
+#     publication_date = serializers.DateField()
+#     isnb = serializers.CharField()
+#     publisher = PublisherSerializer()
     
 
 class ContributorSerializer(serializers.Serializer):
