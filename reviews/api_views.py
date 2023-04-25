cab413dfa74fa3207f4284f1753c61824e92bad4
ehuntober <ehuntercrypto@gmail.com>
2023-04-25 13:31:25 +0000

    view added

--- reviews/api_views.py
+++ reviews/api_views.py
@@ -4,14 +4,21 @@ from rest_framework.response import Response
 from .models import Book 
 from .serializers import BookSerializer
 
+from rest_framework import generics
+
 @api_view()
 def first_api_view(request):
     num_books = Book.objects.count()
     return Response({"num_books": num_books})
 
 
-@api_view()
-def all_books(request):
-    books = Book.objects.all()
-    book_serializer = BookSerializer(books,many=True)
-    return Response(book_serializer.data)
+# @api_view()
+# def all_books(request):
+#     books = Book.objects.all()
+#     book_serializer = BookSerializer(books,many=True)
+#     return Response(book_serializer.data)
+
+
+class AllBooks(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
