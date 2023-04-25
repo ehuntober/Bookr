17ac841ba71d1a7cfa60a2744c62a26959f638a4
ehuntober <ehuntercrypto@gmail.com>
2023-04-25 13:33:22 +0000

    set up URL for classbased view

--- reviews/urls.py
+++ reviews/urls.py
@@ -18,7 +18,8 @@ urlpatterns = [
     
     # api urls
     path('api/first_api_view/', api_views.first_api_view),
-    path('api/all_books/', api_views.all_books, name='all_books'),
+    # path('api/all_books/', api_views.all_books, name='all_books'),
+    path('api/all_books/', api_views.AllBooks.as_view(),name='all_books')
     
     
 ]
