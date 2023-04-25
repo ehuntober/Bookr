334ac9312de065d20dfd1295e97eb69fdfad6c28
ehuntober <ehuntercrypto@gmail.com>
2023-04-25 14:20:59 +0000

    api_view modified

--- reviews/api_views.py
+++ reviews/api_views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
 
-from .models import Book 
+from .models import Book , Contributor
 from .serializers import BookSerializer , ContributorSerializer
 
 from rest_framework import generics
