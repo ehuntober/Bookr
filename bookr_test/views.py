e8bc60822493bdd3cdbb718a15845cb6596f5db0
ehuntober <ehuntercrypto@gmail.com>
2023-04-26 05:45:39 +0000

    test added

--- bookr_test/views.py
+++ bookr_test/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def greeting_view(request):
+    """Greet the user"""
+    return HttpResponse("Hey there, welcome to Bookr!, Your one stop place to review books")
